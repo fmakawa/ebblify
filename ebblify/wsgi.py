b3941be6c01f79b40b2c652f38ac7d6070ea2db6
Fungai Makawa <fungai.makawa@gmail.com>
2019-07-30 13:12:41 +0200

    adjusted wsgi

--- ebblify/wsgi.py
+++ ebblify/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loginAndMessaging.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ebblify.settings')
 
 application = get_wsgi_application()
